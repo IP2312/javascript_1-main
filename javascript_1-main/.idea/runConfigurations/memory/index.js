a8fc7e9221ab812d80616e955a8c201fc8109edb
iason <iason.pifeas@gmail.com>
2025-06-25 15:21:01 +0200

    25.06

--- javascript_1-main/.idea/runConfigurations/memory/index.js
+++ javascript_1-main/.idea/runConfigurations/memory/index.js
@@ -1,13 +1,29 @@
+let nrOfTurndCards = 0;
+const imagesPath= [
+    'images/canoe.png',
+    'images/tiger.png',
+    'images/cat.png',
+    'images/hawaiian-shirt.png',
+    'images/lion.png',
+    'images/monkey.png',
+    'images/mushroom.png',
+    'images/pamela-hat',
+    'images/resort.png',
+    'images/rice.png',
+    'images/sand-castle.png']
 
+function turnCard(card, path){
+    console.log("turnCard", card);
 
-function turnCard(cardNo){
-    console.log("turnCard" + cardNo);
-
-    if(document.querySelector(`#card${cardNo}`).innerHTML ===`${cardNo}`){
-        document.querySelector(`#card${cardNo}`).innerHTML =  cardNo + " was turned";
-    } else{
-        document.querySelector(`#card${cardNo}`).innerHTML = cardNo;
+    if (!card.classList.contains("flipped")){
+        img = card.querySelector("img");
+        img.src = imagesPath[0];
+        card.classList.add("flipped");
+        nrOfTurndCards++;
     }
+
+
+
 
 
 }
