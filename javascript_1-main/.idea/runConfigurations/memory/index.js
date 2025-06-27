f82ae4a94038fadd384d2ce931662db5fd29a6c3
iason <iason.pifeas@gmail.com>
2025-06-27 12:19:40 +0200

    27.06

--- javascript_1-main/.idea/runConfigurations/memory/index.js
+++ javascript_1-main/.idea/runConfigurations/memory/index.js
@@ -1,4 +1,4 @@
-let nrOfTurndCards = 4;
+let nrOfTurndCards = 0;
 const imagesPath= [
     'images/canoe.png',
     'images/cat.png',
@@ -6,19 +6,21 @@ const imagesPath= [
     'images/lion.png',
     'images/monkey.png',
     'images/mushroom.png',
-    'images/pamela-hat',
+    'images/pamela-hat.png',
     'images/resort.png',
     'images/rice.png',
     'images/sand-castle.png']
 
-
+let nrOfCards = imagesPath.length * 2;
+//(let randomPairs = pickPairRandom(imagesPath.length * 2)
 
 
 document.addEventListener("DOMContentLoaded", function() {
     console.log("loaded!");
-    for (let i = 0; i < nrOfTurndCards; i++) {
+    for (let i = 0; i < nrOfCards; i++) {
         createCard();
     }
+    console.log(pickPairRandom(imagesPath.length))
 
 })
 
@@ -28,10 +30,16 @@ let cardDeck = document.getElementsByClassName('cardDeck');
 
 const createCard = function (){
     let newCard = document.createElement('button');
-    newCard.innerHTML= 'new Button';
+    let newImg = document.createElement('img');
+    newImg.src = "images/card-back.jpg";
+    newImg.alt = "card"
+    newCard.appendChild(newImg);
+    newCard.onclick = function (){
+        turnCard(this,imagesPath[Math.floor(Math.random()*imagesPath.length)]);
+    }
+    newCard.classList.add('card');
     cardDeck[0].appendChild(newCard);
 
-    let currentElement = cardDeck[0].children;
 
 }
 
@@ -47,6 +55,25 @@ function turnCard(card, path){
         card.classList.add("flipped");
         nrOfTurndCards++;
     }
+}
 
+// pickPairRandom
+function pickPairRandom(decksize){
+    randomPairs = new Array(decksize);
+    for (let i = 0; i < decksize; i ++){
+        randomPairs[i * 2] = i;
+        randomPairs[i * 2 + 1] = i;
 
+    }
+
+    //Fisher_Yates_Shuffle
+    for (let i = randomPairs.length - 1; i > 0; i--){
+        let randomIndex = Math.floor(Math.random() * (i + 1));
+        let temp = randomPairs[i];
+        randomPairs[i] = randomPairs[randomIndex];
+        randomPairs[randomIndex] = temp;
+
+    }
+
+    console.log(randomPairs);
 }
