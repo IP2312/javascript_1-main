30af498ad63c878017caaca706bfa0d4f6e6b51e
iason <iason.pifeas@gmail.com>
2025-06-30 13:35:45 +0200

    30.06

--- javascript_1-main/.idea/runConfigurations/memory/index.js
+++ javascript_1-main/.idea/runConfigurations/memory/index.js
@@ -1,5 +1,5 @@
 let nrOfTurndCards = 0;
-const imagesPath= [
+const imagesPath = [
     'images/canoe.png',
     'images/cat.png',
     'images/hawaiian-shirt.png',
@@ -12,68 +12,98 @@ const imagesPath= [
     'images/sand-castle.png']
 
 let nrOfCards = imagesPath.length * 2;
-//(let randomPairs = pickPairRandom(imagesPath.length * 2)
 
-
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     console.log("loaded!");
-    for (let i = 0; i < nrOfCards; i++) {
-        createCard();
-    }
-    console.log(pickPairRandom(imagesPath.length))
+    let randomPairs = pickPairRandom(imagesPath.length);
 
+    for (let i = 0; i < nrOfCards; i++) {
+        createCard(imagesPath[randomPairs[i]]);
+    }
 })
+
+
 
 //create new Button
 let card = document.getElementsByClassName('card');
 let cardDeck = document.getElementsByClassName('cardDeck');
 
-const createCard = function (){
+function createCard(path) {
     let newCard = document.createElement('button');
     let newImg = document.createElement('img');
     newImg.src = "images/card-back.jpg";
     newImg.alt = "card"
     newCard.appendChild(newImg);
-    newCard.onclick = function (){
-        turnCard(this,imagesPath[Math.floor(Math.random()*imagesPath.length)]);
+    newCard.onclick = function () {
+        turnCard(this, path);
     }
     newCard.classList.add('card');
     cardDeck[0].appendChild(newCard);
-
-
 }
 
 
+function turnCard(card, path) {
 
-function turnCard(card, path){
-    console.log("turnCard", card);
-    console.log(card);
-
-    if (!card.classList.contains("flipped")){
+    if (!card.classList.contains("flipped")) {
         imgage = card.querySelector("img");
         imgage.src = path;
         card.classList.add("flipped");
         nrOfTurndCards++;
+
+        if (nrOfTurndCards === 2){
+            samePair();
+            nrOfTurndCards = 0;
+        }
     }
 }
 
 // pickPairRandom
-function pickPairRandom(decksize){
+function pickPairRandom(decksize) {
     randomPairs = new Array(decksize);
-    for (let i = 0; i < decksize; i ++){
+    for (let i = 0; i < decksize; i++) {
         randomPairs[i * 2] = i;
         randomPairs[i * 2 + 1] = i;
-
     }
 
     //Fisher_Yates_Shuffle
-    for (let i = randomPairs.length - 1; i > 0; i--){
+    for (let i = randomPairs.length - 1; i > 0; i--) {
         let randomIndex = Math.floor(Math.random() * (i + 1));
         let temp = randomPairs[i];
         randomPairs[i] = randomPairs[randomIndex];
         randomPairs[randomIndex] = temp;
+    }
+    console.log(randomPairs)
+    return randomPairs;
+}
+
+let matches = 0;
+function samePair(){
+    let card1 = document.getElementsByClassName('card flipped')[0];
+    let card2 = document.getElementsByClassName('card flipped')[1] ;
+
+
+    if (card1.querySelector("img").src === card2.querySelector("img").src){
+        console.log("pair Found!")
+        console.log(card1.querySelector("img").src);
+        card1.classList.add("match");
+        console.log(card1);
+        matches += 2;
+
+        if (matches === nrOfCards){
+            alert("You Won!")
+        }
+
+        }
+    else{
+
+        setTimeout(function (){
+            card1.classList.remove("flipped");
+            console.log("removed")
+            card1.querySelector("img").src = "images/card-back.jpg"
+            card2.classList.remove("flipped");
+            card2.querySelector("img").src = "images/card-back.jpg";
+        },1000)
 
     }
 
-    console.log(randomPairs);
 }
